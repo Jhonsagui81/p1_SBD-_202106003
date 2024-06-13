af02e34149d2cd07811ad35133cfb56555fe2104
Jhonsagui81 <reyesjhonatan86@gmail.com>
2024-06-12 21:26:08 -0600

    listo

--- caso3/conceptual.sql
+++ caso3/conceptual.sql
@@ -29,13 +29,70 @@
 -- Par_codigo
 -- Par_nombre
 -- Par_color
+-- Par_tipo
 -- Par_garantia
 -- Par_costo
 -- Par_pro_id (foranea)
--- Par_tp_id (foranea)
 
 
 -- ----------------------------------------
 -- Producto 
 -- ----------------------------------------
--- 
+-- Pro_id (#)
+-- Pro_codigo
+-- Pro_nombre
+-- Pro_marca
+-- Pro_fecha finalizo
+-- Pro_costo_fabricacion (manoobra y maquinaria)
+-- Pro_costo_verificacion
+-- Pro_costo_partes
+-- Pro_id_empleado_finalizo (foranea)
+-- Pro_id_puesto_finalizado (foranea)
+-- Pro_id_bodega (foranea)
+
+
+-- ----------------------------------------
+-- tipo_linea
+-- ----------------------------------------
+-- tl_id (#)
+-- tl_descripcion (telefono, computadora)
+
+-- ----------------------------------------
+-- Linea_samblaje
+-- ----------------------------------------
+-- Lin_id (#)
+-- Lin_//varios puestos de trabajo
+-- Lin_pro_id (foranea)
+-- Lin_tl_id (foranea)
+
+
+-- ----------------------------------------
+-- Empleados
+-- ----------------------------------------
+-- id
+-- codigo
+-- nombre
+-- direccion
+-- telefono
+-- correo
+-- pues_id (operario, jefe)
+
+-- ----------------------------------------
+-- puesto_trabajo
+-- ----------------------------------------
+-- id (#)
+-- descripcion
+-- lin_id (foranea)
+
+
+-- ----------------------------------------
+-- Empleado_puesto_trabajo
+-- ----------------------------------------
+-- id (#)
+-- fecha_inicio
+-- fecha_fin
+-- emp_id (foranea)
+-- puesto_id (foranea)
+
+
+
