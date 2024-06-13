b2989745d0dc8c7878e289de9a2411f0ff734525
Jhonsagui81 <reyesjhonatan86@gmail.com>
2024-06-12 19:39:36 -0600

    primeros pasos

---
+++ caso3/conceptual.sql
@@ -0,0 +1,41 @@
+-- ----------------------------------------
+-- Bodega
+-- ----------------------------------------
+-- bod_id (#)
+-- bod_encargado
+-- bod_direccion
+-- bod_stock_productos
+-- bod_tb_id (foranea)
+
+-- ----------------------------------------
+-- tipo_bodega
+-- ----------------------------------------
+-- tb_id (#)
+-- tb_descripcion (partes, productos terminados, media, reclamos)
+
+-- ----------------------------------------
+-- tipo_etapas
+-- ----------------------------------------
+-- id (#)
+-- etapa (inicio, intermedio, finalizacion)
+-- costo_funcionamiento
+-- id_producto
+
+
+-- ----------------------------------------
+-- Partes
+-- ----------------------------------------
+-- Par_id (#)
+-- Par_codigo
+-- Par_nombre
+-- Par_color
+-- Par_garantia
+-- Par_costo
+-- Par_pro_id (foranea)
+-- Par_tp_id (foranea)
+
+
+-- ----------------------------------------
+-- Producto 
+-- ----------------------------------------
+-- 
